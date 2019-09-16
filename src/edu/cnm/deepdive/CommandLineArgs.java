59a34294a4ff21efe1604df8f441d03e862cad6f
Sami <samimheard@gmail.com>
2019-09-16 11:39:44 -0600

    Changed Lower case

--- src/edu/cnm/deepdive/CommandLineArgs.java
+++ src/edu/cnm/deepdive/CommandLineArgs.java
@@ -6,7 +6,7 @@ public class CommandLineArgs {
 
   public static void main(String[] args) {
     for (String arg : args){
-      System.out.println(arg.toUpperCase());
+      System.out.println(arg.toLowerCase());
     }
     System.out.println(Arrays.toString(args));
   }
